5d9f239205970018640d7f238415b1d5eb7a245c
Fly-egg8 <91406213+Fly-egg8@users.noreply.github.com>
2022-06-24 16:34:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function text(){
+document.querySelector("link").href ="main 2.css"
+
+}	
+
+function changePlace() {
+	 alert('не копируй')
+	//document.querySelector('#old-place').classList = 'new-place';
+}
